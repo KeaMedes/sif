35673fb1090b039f0551858394368d8d0289cfd1
KeaMedes <KeaMedes@gmail.com>
2014-11-03 23:34:44 +0800

    have a try for branch

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /**
  * Outer tester of the whole project
  * @author luo yang
@@ -6,5 +8,8 @@
 public class Test {
 public static void main(String[] args){
 	System.out.println("fuck you");
+	List<String> names = new HashList<String>();
+}
+private void geta(int a){
 }
 }
